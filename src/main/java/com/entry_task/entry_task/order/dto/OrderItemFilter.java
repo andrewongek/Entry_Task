1c65a02821c79b03f34c0d494c4b0413d5c0079b
andrewongek <123345790+andrewongek@users.noreply.github.com>
2025-12-17 18:32:06 +0800

    implement admin and seller search logic

---
+++ src/main/java/com/entry_task/entry_task/order/dto/OrderItemFilter.java
@@ -0,0 +1,31 @@
+package com.entry_task.entry_task.order.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+public record OrderItemFilter(
+
+        @Schema(description = "keyword for search of product name")
+        String keyword,
+
+        @Schema(description = "Filter by userId of the buyer")
+        Long userId,
+
+        @Schema(description = "Filter by orderId of the Order")
+        Long orderId,
+
+        @Schema(description = "Get orderItem by the id")
+        Long orderItemId,
+
+        @Schema(
+                description = "List of order statuses to filter the results by. " +
+                        "Each value must be one of the OrderStatus enum values: " +
+                        "CREATED, PAID, SHIPPED, COMPLETED, CANCELLED. " +
+                        "If multiple statuses are provided, orders matching any of the statuses will be returned.",
+                example = "[\"PAID\", \"SHIPPED\"]"
+        )
+        List<String> statuses
+
+) {
+}
--- src/main/java/com/entry_task/entry_task/order/service/OrderService.java
+++ src/main/java/com/entry_task/entry_task/order/service/OrderService.java
@@ -13,6 +13,7 @@ import com.entry_task.entry_task.exceptions.ProductNotFoundException;
 import com.entry_task.entry_task.order.dto.*;
 import com.entry_task.entry_task.order.entity.Order;
 import com.entry_task.entry_task.order.entity.OrderItem;
+import com.entry_task.entry_task.order.specifications.OrderItemSpecifications;
 import com.entry_task.entry_task.product.entity.Product;
 import com.entry_task.entry_task.order.repository.OrderItemRepository;
 import com.entry_task.entry_task.order.repository.OrderRepository;
@@ -23,6 +24,7 @@ import jakarta.persistence.OptimisticLockException;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -41,12 +43,14 @@ public class OrderService {
     private static final Logger log = LoggerFactory.getLogger(OrderService.class);
 
     private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
     private final AuthService authService;
     private final CartService cartService;
     private final ProductService productService;
 
-    public OrderService(OrderRepository orderRepository, AuthService authService, CartService cartService, ProductService productService) {
+    public OrderService(OrderRepository orderRepository, OrderItemRepository orderItemRepository, AuthService authService, CartService cartService, ProductService productService) {
         this.orderRepository = orderRepository;
+        this.orderItemRepository = orderItemRepository;
         this.authService = authService;
         this.cartService = cartService;
         this.productService = productService;
@@ -186,5 +190,128 @@ public class OrderService {
                 order.getmTime()
         );
     }
+
+    @PreAuthorize("hasRole('SELLER')")
+    public OrderInvoiceItemListResponse getSellerOrderInvoiceItemList(SellerOrderInvoiceItemListRequest request) {
+        return getSellerOrderInvoiceItemList(request, authService.getCurrentUser().getId());
+    }
+
+    @Cacheable(
+            value = "order_invoice_item:list",
+            key = "T(java.util.Objects).hash(#request, #sellerId)"
+    )
+    private OrderInvoiceItemListResponse getSellerOrderInvoiceItemList(SellerOrderInvoiceItemListRequest request, Long sellerId) {
+        Sort.Direction sortDirection =
+                Sort.Direction.fromString(request.sort().order());
+
+        Pageable pageable = PageRequest.of(
+                request.pagination().page(),
+                request.pagination().size(),
+                Sort.by(sortDirection, request.sort().field())
+        );
+
+        Specification<OrderItem> spec = OrderItemSpecifications.belongsToSeller(sellerId);
+        if (request.filter() != null) {
+            if (request.filter().keyword() != null && !request.filter().keyword().isBlank()) {
+                spec = spec.and(OrderItemSpecifications.productNameContains(request.filter().keyword()));
+            }
+            if (request.filter().userId() != null) {
+                spec = spec.and(OrderItemSpecifications.userIdEquals(request.filter().userId()));
+            }
+            if (request.filter().orderId() != null) {
+                spec = spec.and(OrderItemSpecifications.orderIdEquals(request.filter().orderId()));
+            }
+            if (request.filter().orderItemId() != null) {
+                spec = spec.and(OrderItemSpecifications.orderItemIdEquals(request.filter().orderItemId()));
+            }
+            if (request.filter().statuses() != null && !request.filter().statuses().isEmpty()) {
+                spec = spec.and(OrderItemSpecifications.orderStatusIn(request.filter().statuses()));
+            }
+        }
+
+        Page<OrderInvoiceItemResponse> page = orderItemRepository.findAll(spec, pageable).map(orderItem -> new OrderInvoiceItemResponse(
+                orderItem.getId(),
+                orderItem.getOrder().getId(),
+                orderItem.getOrder().getId(),
+                orderItem.getProduct().getId(),
+                orderItem.getProduct().getName(),
+                orderItem.getQuantity(),
+                orderItem.getPrice(),
+                orderItem.getQuantity() * orderItem.getPrice(),
+                orderItem.getOrder().getStatus().name(),
+                orderItem.getOrder().getcTime(),
+                orderItem.getOrder().getmTime()
+        ));
+
+        return new OrderInvoiceItemListResponse(
+                page.toList(),
+                new Metadata(
+                        page.getTotalElements(),
+                        page.getNumber(),
+                        page.getSize(),
+                        page.hasNext()
+                )
+        );
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public OrderInvoiceItemListResponse getOrderInvoiceItemList(AdminOrderInvoiceItemListRequest request) {
+        Sort.Direction sortDirection =
+                Sort.Direction.fromString(request.sort().order());
+
+        Pageable pageable = PageRequest.of(
+                request.pagination().page(),
+                request.pagination().size(),
+                Sort.by(sortDirection, request.sort().field())
+        );
+
+        Specification<OrderItem> spec = Specification.unrestricted();
+        if (request.sellerId() != null) {
+            return getSellerOrderInvoiceItemList(new SellerOrderInvoiceItemListRequest(request.pagination(), request.filter(), request.sort()), request.sellerId());
+        }
+        if (request.filter() != null) {
+            if (request.filter().keyword() != null && !request.filter().keyword().isBlank()) {
+                spec = spec.and(OrderItemSpecifications.productNameContains(request.filter().keyword()));
+            }
+            if (request.filter().userId() != null) {
+                spec = spec.and(OrderItemSpecifications.userIdEquals(request.filter().userId()));
+            }
+            if (request.filter().orderId() != null) {
+                spec = spec.and(OrderItemSpecifications.orderIdEquals(request.filter().orderId()));
+            }
+            if (request.filter().orderItemId() != null) {
+                spec = spec.and(OrderItemSpecifications.orderItemIdEquals(request.filter().orderItemId()));
+            }
+            if (request.filter().statuses() != null && !request.filter().statuses().isEmpty()) {
+                spec = spec.and(OrderItemSpecifications.orderStatusIn(request.filter().statuses()));
+            }
+        }
+
+        Page<OrderInvoiceItemResponse> page = orderItemRepository.findAll(spec, pageable).map(orderItem -> new OrderInvoiceItemResponse(
+                orderItem.getId(),
+                orderItem.getOrder().getId(),
+                orderItem.getOrder().getId(),
+                orderItem.getProduct().getId(),
+                orderItem.getProduct().getName(),
+                orderItem.getQuantity(),
+                orderItem.getPrice(),
+                orderItem.getQuantity() * orderItem.getPrice(),
+                orderItem.getOrder().getStatus().name(),
+                orderItem.getOrder().getcTime(),
+                orderItem.getOrder().getmTime()
+        ));
+
+        return new OrderInvoiceItemListResponse(
+                page.toList(),
+                new Metadata(
+                        page.getTotalElements(),
+                        page.getNumber(),
+                        page.getSize(),
+                        page.hasNext()
+                )
+        );
+
+    }
+
 }
 
