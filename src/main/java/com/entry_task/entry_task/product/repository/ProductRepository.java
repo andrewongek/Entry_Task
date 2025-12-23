1456257c453248148b6f528a3d5f724743e3f3fc
andrewongek <123345790+andrewongek@users.noreply.github.com>
2025-12-23 10:03:51 +0800

    add entitygraphs

--- src/main/java/com/entry_task/entry_task/product/repository/ProductRepository.java
+++ src/main/java/com/entry_task/entry_task/product/repository/ProductRepository.java
@@ -1,20 +1,29 @@
 package com.entry_task.entry_task.product.repository;
 
 import com.entry_task.entry_task.product.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("""
-        UPDATE Product p
-        SET p.stock = p.stock - :qty
-        WHERE p.id = :id
-          AND p.stock >= :qty
-          AND p.productStatus = com.entry_task.entry_task.enums.ProductStatus.ACTIVE
-    """)
+                UPDATE Product p
+                SET p.stock = p.stock - :qty
+                WHERE p.id = :id
+                  AND p.stock >= :qty
+                  AND p.productStatus = com.entry_task.entry_task.enums.ProductStatus.ACTIVE
+            """)
     int reserveStock(@Param("id") long id, @Param("qty") int qty);
+
+    @EntityGraph(attributePaths = "seller")
+    @NonNull
+    Page<Product> findAll(
+            @Nullable Specification<Product> spec,
+            @NonNull Pageable pageable
+    );
 }
